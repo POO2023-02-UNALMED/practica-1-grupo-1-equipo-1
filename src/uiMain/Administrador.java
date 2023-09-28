30876637d0ea73f3d06cd0abe6739073564d5593
Mnico-11 <141962291+Mnico-11@users.noreply.github.com>
2023-09-27 22:22:32 -0500

    .-.

--- src/uiMain/Administrador.java
+++ src/uiMain/Administrador.java
@@ -2,6 +2,26 @@ package uiMain;
 
 public class Administrador {
     public static void main(String[] args) {
-        
+    	Administrador admi = new Administrador();
+    	int opcion;
+    	do {
+    		
+    		System.out.println("Bienvenido al Gestor del Restaurante"); // agregar el método getNombre
+    		System.out.println("¿Que función deseas usar?");
+    		System.out.println("1. Gestión de Reserva");
+    		System.out.println("2. Gestión de Pedidos");
+    		System.out.println("3. Gestión de Empleados");
+    		System.out.println("4. Gestión del Inventario");
+    		System.out.println("5. Gestión Financiera");
+    		System.out.print("Escribe el número de la opción que necesitas: ");
+    		opcion = (int) readLong();
+    		
+    		switch(opcion) {
+    		case 1: (admi); break
+    		
+    		}
+    		}
+    		
+    		
     }
 }
