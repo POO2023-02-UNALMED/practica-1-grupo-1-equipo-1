8cecdde91223653001452f2c0408fa36b8c32815
SebastianHoyoss <141962219+SebastianHoyoss@users.noreply.github.com>
2023-10-12 18:09:50 -0500

    reset base de datos

--- src/baseDatos/Serializador.java
+++ src/baseDatos/Serializador.java
@@ -10,63 +10,6 @@ import gestorAplicacion.Cosas.*;
 import gestorAplicacion.Personas.*;
 
 public class Serializador {
-	public static void serializar(GestionDatos gest) {
-		serializarClientes(gest, new File("src\\baseDatos\\temp\\Clientes.txt"));
-		serializarEmpleados(gest, new File("src\\baseDatos\\temp\\Empleados.txt"));
-		serializarMesas(gest, new File("src\\baseDatos\\temp\\Mesas.txt"));
-		serializarReservas(gest, new File("src\\baseDatos\\temp\\Reservas.txt"));
-	}
-		public static void serializarMesas(GestionDatos gest,File ruta) {
-			try {
-				PrintWriter pw= new PrintWriter(ruta);
-				FileOutputStream file=new FileOutputStream(ruta);
-				ObjectOutputStream output=new ObjectOutputStream(file);
-				output.writeObject(gest.getMesas());
-				output.close();
-				file.close();
-			}catch (IOException excepcion) {
-				System.out.println("Error en la serializacion" + excepcion);
-			}
-		}
-		public static void serializarClientes(GestionDatos gest,File ruta) {
-			try {
-				PrintWriter pw= new PrintWriter(ruta);
-				FileOutputStream file=new FileOutputStream(ruta);
-				ObjectOutputStream output=new ObjectOutputStream(file);
-				output.writeObject(gest.getClientes());
-				output.close();
-				file.close();
-			}catch (IOException excepcion) {
-				System.out.println("Error en la serializacion" + excepcion);
-			}
-		}
-		public static void serializarEmpleados(GestionDatos gest,File ruta) {
-			try {
-				PrintWriter pw= new PrintWriter(ruta);
-				FileOutputStream file=new FileOutputStream(ruta);
-				ObjectOutputStream output=new ObjectOutputStream(file);
-				output.writeObject(gest.getEmpleados());
-				output.close();
-				file.close();
-			}catch (IOException excepcion) {
-				System.out.println("Error en la serializacion" + excepcion);
-			}
-		}
-		public static void serializarReservas(GestionDatos gest,File ruta) {
-			for (Mesa mesas:gest.getRestaurante().getMesas()) {
-				try {
-					PrintWriter pw= new PrintWriter(ruta);
-					FileOutputStream file=new FileOutputStream(ruta);
-					ObjectOutputStream output=new ObjectOutputStream(file);
-					output.writeObject(mesas.getReserva());
-					output.close();
-					file.close();
-				}catch (IOException excepcion) {
-					System.out.println("Error en la serializacion" + excepcion);
-				}
-			}
-			
-		}
-		
+	
 	
 }
