e77d58305379f1fcc1a6a81abfae75dee45e56db
JhogertBita <141960659+JhogertBita@users.noreply.github.com>
2023-10-22 22:43:35 -0500

    Create Mesero.java

---
+++ src/gestorAplicacion/Personas/Mesero.java
@@ -0,0 +1,27 @@
+package gestorAplicacion.Personas;
+
+import java.io.Serializable;
+
+public class Mesero extends Empleado implements Serializable{
+  private static final long serialVersionUID = 1L;
+
+  public Mesero() { }
+  public Mesero(String nombre, Long cedula, String puesto, Restaurante restaurante, Turno turno){
+      super(nombre, cedula, puesto, restaurante, turno);
+   // Inicializa la lista de turnos
+      this.turnos = new ArrayList<>();
+   // Añade el turno a la lista
+      this.turnos.add(turno); 
+      this.setFechaContratacion(new Date()); 
+  }
+
+  //Metodo para mostrar la puntuacion del Empleado
+  public String puntuacion(){
+    return "La puntacion del Mesero es: "+ this.getPuntuacion();
+  }
+
+  //Metodo para mostrar la descripcion del trabajo del Empleado
+  public String trabajo(){
+    return "Mesero, es quien se encarga de atender y servir a la clientela."
+  }
+}
