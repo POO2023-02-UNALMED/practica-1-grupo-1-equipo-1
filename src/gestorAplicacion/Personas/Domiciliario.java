d5b7d77438c88b1450ca35b6341ab1d8a9eeba2c
JhogertBita <141960659+JhogertBita@users.noreply.github.com>
2023-10-22 22:43:02 -0500

    Create Domiciliario.java

---
+++ src/gestorAplicacion/Personas/Domiciliario.java
@@ -0,0 +1,27 @@
+package gestorAplicacion.Personas;
+
+import java.io.Serializable;
+
+public class Domiciliario extends Empleado implements Serializable{
+  private static final long serialVersionUID = 1L;
+
+  public Domiciliario() { }
+  public Domiciliario(String nombre, Long cedula, String puesto, Restaurante restaurante, Turno turno){
+      super(nombre, cedula, puesto, restaurante, turno);
+   // Inicializa la lista de turnos
+      this.turnos = new ArrayList<>();
+   // Añade el turno a la lista
+      this.turnos.add(turno); 
+      this.setFechaContratacion(new Date()); 
+  }
+
+  //Metodo para mostrar la puntuacion del Empleado
+  public String puntuacion(){
+    return "La puntacion del Domiciliario es: "+ this.getPuntuacion();
+  }
+
+  //Metodo para mostrar la descripcion del trabajo del Empleado
+  public String trabajo(){
+    return "Domiciliario, es la persona encargada de entregar los pedidos a domicilio."
+  }
+}
